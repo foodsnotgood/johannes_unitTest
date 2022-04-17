8c48379512c6b2fd3b7024878f4963cf841c43c3
verapeeters-thomasmore <vera.peeters@thomasmore.be>
2022-04-17 18:30:01 +0200

    first example

---
+++ src/main/java/ex01_arrays/ArrayOfInts.java
@@ -0,0 +1,20 @@
+package ex01_arrays;
+
+import java.util.Arrays;
+
+public class ArrayOfInts {
+
+    public int sumOfInts(int[] arrayOfInts) {
+        if (arrayOfInts == null) return 0;
+        int sum = 0;
+        for (int i : arrayOfInts)
+            sum += i;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ArrayOfInts ex1 = new ArrayOfInts();
+        int result = ex1.sumOfInts(new int[]{1, 2, 3, 4});
+        System.out.println(result);
+    }
+}
